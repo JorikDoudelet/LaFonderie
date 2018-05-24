df6903ee823bb1c5dcc912f6b89b46df5207fca6
Gunnar Hemmeryckx <gunnar.hemmeryckx@student.ehb.be>
2018-05-24 10:02:05 +0200

    Initial Commit

---
+++ LaFonderie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaFonderie
+//
+//  Created by Gunnar Hemmeryckx on 24/05/18.
+//  Copyright © 2018 Gunnar Hemmeryckx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
